f3cf0a9e0070cf527067003d0d52ca17948b63bc
Mederic Guillen Moya <thelegendofsnake@hotmail.fr>
2020-08-13 16:55:23 +0200

    Debut du MVC index(Request)

---
+++ Template/RequestBookChapter.php
@@ -0,0 +1,13 @@
+<?php
+function getBillet()
+{
+	try{
+		$bdd = new PDO('mysql:host=localhost;dbname=livre;charset=utf8', 'root', 'root');
+	}
+	catch(Execption $e){
+		die("Erreur : " . $e->getMessage());
+	}
+	$Chapter = $bdd->query('SELECT * FROM livre ORDER BY NumeroChapitre');
+	return $Chapter;
+}
+?>
